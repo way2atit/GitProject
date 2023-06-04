fd47b3e0e33a46107303e7806dd5f23e45f93ec2
way2atit <way2atit@gmail.com>
2023-06-04 11:54:31 +0530

    Login test method added

--- src/test/java/com/selenium/SeleniumGitProject/Test1.java
+++ src/test/java/com/selenium/SeleniumGitProject/Test1.java
@@ -2,10 +2,14 @@
 package com.selenium.SeleniumGitProject;
 
 public class Test1 {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("Test1 class");
 	}
 
+	public void logintest() {
+		System.out.println("logintest");
+
+	}
 }
--- src/test/java/com/selenium/SeleniumGitProject/Test2.java
+++ src/test/java/com/selenium/SeleniumGitProject/Test2.java
@@ -1,9 +1,18 @@
 package com.selenium.SeleniumGitProject;
 
 public class Test2 {
-	
+
 	public void m1() {
-		
+
+	}
+
+	public void logintest() {
+
 	}
 
 }
+
+//
+// workspace -- staging -- git local -- git hub
+// test1 test1 test1
+// test2 -- (git add . ) test2 --test2
